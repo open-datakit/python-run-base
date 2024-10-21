70db295f42cb9fb9dc1c0fcb4556e7dcee9b503d
Varvara Efremova <varvara@echus.co>
2024-10-21 11:21:34 +1100

    Add version tags to build, release v0.1.2

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t opends/python-run-base:v1 . $@
+docker build -t opends/python-run-base:v0 -t opends/python-run-base:v0.1.2 -t opends/python-run-base:latest . $@
